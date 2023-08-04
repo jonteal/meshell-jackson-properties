0ce9a92bf134302f12a7d2e3aac3635c070a771e
Jon Jackson <jonjacksonvibes@gmail.com>
2023-08-03 22:13:10 -0500

    removed footer temporarily

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import Footer from './components/footer/Footer';
-import Navigation from './routes/navigation/Navigation';
-import Home from './routes/home/Home';
-import About from './routes/about/About';
-import Contact from './routes/contact/Contact';
-import Rentals from './routes/rentals/Rentals';
-import Management from './routes/management/Management';
-import Faq from './routes/faq/Faq';
+import Footer from "./components/footer/Footer";
+import Navigation from "./routes/navigation/Navigation";
+import Home from "./routes/home/Home";
+import About from "./routes/about/About";
+import Contact from "./routes/contact/Contact";
+import Rentals from "./routes/rentals/Rentals";
+import Management from "./routes/management/Management";
+import Faq from "./routes/faq/Faq";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Navigation />}>
-          <Route index element={<Home />}/>
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='rentals' element={<Rentals />} />
-          <Route path='management' element={<Management />} />
-          <Route path='faq' element={<Faq />} />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="rentals" element={<Rentals />} />
+          <Route path="management" element={<Management />} />
+          <Route path="faq" element={<Faq />} />
         </Route>
       </Routes>
-    <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
